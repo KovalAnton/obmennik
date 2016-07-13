3808c07f934322ebd878040f6ff3b577f662ddac
Антон <antonkoval@MacBook-Pro-Anton.local>
2016-07-14 04:03:46 +0800

    рабочее мобильное меню

--- app/js/common.js
+++ app/js/common.js
@@ -26,3 +26,57 @@ $(function() {
 	});
 
 });
+
+$(window).load(function(){
+	if(Cookies.get("full_version") == "12345"){
+		$("#polnay_stranica").css("display","none");
+	} else {
+		$("#mobilnay_stranica").css("display","none");
+	}
+});
+
+$(document).ready(function() {
+
+	$("#polnay_stranica").click(function(){
+		Cookies.set('full_version', '12345', {
+			expires: 1,
+			path: "/",
+		});
+		location.reload();
+	});
+
+	$("#mobilnay_stranica").click(function(){
+		Cookies.remove("full_version");
+		location.reload();
+	});
+
+
+	$("nav ul").clone([],[true]).appendTo(".mobile_navigation");
+	$(".mobile_navigation ul li").removeClass("active_nav");
+	$(".regestration").clone().appendTo(".mobile_navigation");
+
+	$(".mobile_navigation ul li").animated("fadeInRight", "fadeOutDown");
+
+
+	$(".nav_button").click(function() {
+		if ($(".mobile_navigation").css("display") == "none") {
+			$(".mobile_navigation").show("slow");
+		} else {
+			$(".mobile_navigation").hide("slow");
+		}
+		$(".mobile_navigation .regestration").css("display", "block");
+	});
+
+	$(".bottom_header a").click(function() {
+		$(".bottom_header li").removeClass("active_nav");
+		$(this).parent().addClass("active_nav");
+	});
+
+	var menu = $('.wrapper div');
+
+	menu.on('click', function() {
+  	var menuNum = $(this).data('menu');
+  	$(this).toggleClass('menu-'+ menuNum +'-active');
+	})
+
+});
